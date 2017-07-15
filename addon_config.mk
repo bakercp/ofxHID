62c0cb1d9a8afecc41b2bffcf4c604f2c53d2b47
Christopher Baker <me@christopherbaker.net>
2017-07-14 23:40:25 -0500

    Fixes

--- addon_config.mk
+++ addon_config.mk
@@ -6,4 +6,29 @@ meta:
 	ADDON_TAGS = "hid" "usb"
 	ADDON_URL = http://github.com/bakercp/ofxHID
 common:
-	ADDON_DEPENDENCIES = ofxIO ofxKinect
+	ADDON_DEPENDENCIES = ofxIO
+
+linux64:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libusb-1.0
+
+linux:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libusb-1.0
+
+msys2:
+	# any library that should be included in the project using pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libusb-1.0
+
+linuxarmv6l:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libusb-1.0
+
+linuxarmv7l:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libusb-1.0
+	
